e9710a1727f66d88ff0f780164b827be666a764e
dono4 <norman-do@hotmail.com>
2025-09-16 12:28:07 -0400

    change1

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Hello World from Azure App Service!');
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
